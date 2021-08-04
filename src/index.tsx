d8ebf6989eafe156e3a3329ee8748106b6a4098c
activeguild <workdrive0306@gmail.com>
2021-08-04 15:36:11 +0900

    Remove unnesesary code

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
 import { render } from "solid-js/web";
-import { liff } from "@line/liff";
-
 import "./index.css";
 import App from "./App";
-import { getLiffId } from "./utils/environment";
 
 var script = document.createElement('script');
 script.src = 'https://static.line-scdn.net/liff/edge/2/sdk.js';
 script.onload= () => {
-  // console.log('liff :>> ', liff);
-  // console.log('getLiffId() :>> ', getLiffId());
-  // console.log('window.liff :>> ', window.liff);
-  window.liff.init({ liffId: getLiffId() }).then((value) => {
     render(() => <App />, document.getElementById("root"));
-  });
 }
 document.body.appendChild(script);
 
